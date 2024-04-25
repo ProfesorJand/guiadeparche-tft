c7f780003f2e7cbd93a41d9d7030e7c6765c47b3
profesorjand <profesorjand@gmail.com>
2024-04-24 21:20:46 -0300

    version 0

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://tft.guiadeparche.com', // site: 'https://profesorjand.github.io'
+});
