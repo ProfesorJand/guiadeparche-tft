62ae0faea39ce302d970ebc890cffd314f124b16
profesorjand <profesorjand@gmail.com>
2024-05-02 21:20:09 -0300

    delete index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tft.guiadeparche.com', // site: 'https://profesorjand.github.io'
+  image: {
+    domains: ['astro.build'],
+  },
+  redirects: {
+    '/': '/composiciones-tft/mejores-composiciones-de-teamfight-tactics/',
+  },
 });
