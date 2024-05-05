99bff2605e8bd0fe32d480deca3aa78298c64d5d
profesorjand <profesorjand@gmail.com>
2024-05-05 16:44:03 -0300

    testing cors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tft.guiadeparche.com', // site: 'https://profesorjand.github.io'
+  site: 'https://tft.guiadeparche.com',
+  // site: 'https://profesorjand.github.io'
   image: {
     domains: ['astro.build'],
   },
   redirects: {
     '/': '/composiciones-tft/mejores-composiciones-de-teamfight-tactics/',
   },
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
