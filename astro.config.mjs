f1be81677831eaed616b7a2710ebdd8deddf0370
profesorjand <profesorjand@gmail.com>
2024-07-11 14:35:19 -0300

    volver a tft.guiadeparche.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  //site: 'https://tft.guiadeparche.com', // para guiadeparche
-  site: 'https://profesorjand.github.io', // para github page
-  base: '/guiadeparche-tft', // para github page
-  trailingSlash: "always", //para github page
+  site: 'https://tft.guiadeparche.com', // para guiadeparche
+  //site: 'https://profesorjand.github.io', // para github page
+  //base: '/guiadeparche-tft', // para github page
+  //trailingSlash: "always", //para github page
   redirects: {
-    '/': '/guiadeparche-tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/'
+    '/': '/composiciones-tft/mejores-composiciones-de-teamfight-tactics/' // github page '/guiadeparche-tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/'
   },
   output: 'static', // Cambiado a 'static' para asegurarse de que se genera contenido estático para deploy
   //output: 'server',
