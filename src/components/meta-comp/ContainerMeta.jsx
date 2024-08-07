6661616b543c6fbd34bb3368e54605d20c7d7a59
profesorjand <profesorjand@gmail.com>
2024-08-07 00:35:45 -0300

    remover comentario en return

--- src/components/meta-comp/ContainerMeta.jsx
+++ src/components/meta-comp/ContainerMeta.jsx
@@ -89,7 +89,6 @@ const ContainerMeta = ({ version, set }) => {
         }
         return null;
       })}
-      {/* <Adsense dimension={"cuadrado"}></Adsense> */}
       <h2 className={[styles.titulo, styles.paddingTop].join(" ")}>Aumentos de Heroes Meta TFT</h2>
       {Object.keys(metaPBE).map((key, index) => {
         if (key.includes("Alternativa")) {
