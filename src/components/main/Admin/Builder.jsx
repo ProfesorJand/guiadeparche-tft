795c9fa1682fc5dd5c1d1fefaed8c32935265134
profesorjand <profesorjand@gmail.com>
2024-08-22 16:24:49 -0300

    añadir hex default y los hex segun tier

--- src/components/main/Admin/Builder.jsx
+++ src/components/main/Admin/Builder.jsx
@@ -1,38 +1,173 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import style from "./css/Builder.module.css";
 const Builder = ({info="", setInfo=""})=>{
     const urlHex = "/hexagonos/"
     const imgHex = ["hex-bronze.webp", "hex-silver.webp", "hex-gold.webp", "hex-prismatic.webp"]
     const [traits, setTraits] = useState({})
     const [campeonesEnBoard, setCampeonesEnBoard] = useState([]);
+    const addSinergia = useCallback((sinergia, contadorActual)=>{
+        setTraits((oldTraits) => {return({...oldTraits,[sinergia]:contadorActual+1})});
+    },[traits]);
+    const addChampionBoard = useCallback(({dataCampeon,hexId})=>{
+        setCampeonesEnBoard((oldArray)=>[...oldArray, {dataCampeon, hexId, campeonId:dataCampeon.nombre}])
+    })
     const championsColor =["var(--color-hex-cost-default)","var(--color-hex-cost-1)","var(--color-hex-cost-2)","var(--color-hex-cost-3)","var(--color-hex-cost-4)","var(--color-hex-cost-5)"]
     const traitsColors = {
+        Arcana:{
+            2:imgHex[0],
+            3:imgHex[1],
+            4:imgHex[2]
+        },
+        Ascendant:{
+            1:imgHex[3],
+        },
+        Bastion:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2],
+            8:imgHex[3]
+        },
+        BatQueen:{
+            1:imgHex[3],
+        },
+        Best_Friends:{
+            1:imgHex[3],
+        },
+        Blaster:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2]
+        },
+        Chrono:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2]
+        },
+        Dragon:{
+            2:imgHex[0],
+            3:imgHex[2],
+        },
+        Druid:{
+            1:imgHex[3],
+        },
+        Eldritch:{
+            3:imgHex[0],
+            5:imgHex[1],
+            7:imgHex[2],
+            10:imgHex[3]
+        },
+        Fearie:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2],
+            9:imgHex[3]
+        },
+        Frost:{
+            3:imgHex[0],
+            5:imgHex[1],
+            7:imgHex[2],
+            9:imgHex[3]
+        },
+        Honeymancy:{
+            3:imgHex[0],
+            5:imgHex[2],
+            7:imgHex[3]
+        },
+        Hunter:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2]
+        },
+        Incantor:{
+            2:imgHex[0],
+            4:imgHex[2],
+        },
+        Mage:{
+            3:imgHex[0],
+            5:imgHex[1],
+            7:imgHex[2],
+            9:imgHex[3]
+        },
+        Multistriker:{
+            3:imgHex[0],
+            5:imgHex[1],
+            7:imgHex[2],
+            9:imgHex[3]
+        },
+        Preserver:{
+            2:imgHex[0],
+            3:imgHex[1],
+            4:imgHex[2],
+            5:imgHex[3]
+        },
+        Portal:{
+            3:imgHex[0],
+            6:imgHex[1],
+            8:imgHex[2],
+            10:imgHex[3]
+        },
+        Pyro:{
+            2:imgHex[0],
+            3:imgHex[1],
+            4:imgHex[2],
+            5:imgHex[3]
+        },
+        Ravenous:{
+            1:imgHex[3],
+        },
         Scholar:{
             2:imgHex[0],
             4:imgHex[1],
             6:imgHex[2]
         },
+        Shapeshifter:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2],
+            8:imgHex[3]
+        },
+        Sugarcraft:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2]
+        },
+        Vanguard:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2],
+        },
         Warrior:{
-            1:imgHex[0],
-            4:imgHex[3]
-        }
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2]
+        },
+        Witchcraft:{
+            2:imgHex[0],
+            4:imgHex[1],
+            6:imgHex[2],
+            8:imgHex[3]
+        },
     }
 
     useEffect(()=>{
-       const keys =  Object.keys(traits);
-       console.log({keys})
-       if(keys.length !== 0){
-        keys.forEach((sinergias)=>{
-            console.log(sinergias)
+        console.log({traits, campeonesEnBoard})
+        Object.keys(traits).forEach((sinergia)=>{
+            const allTraits = document.querySelectorAll(`.${sinergia}`);
+            allTraits.forEach((element)=>{
+                console.log({sinergia, traitsSinergias : traits[sinergia]})
+                console.log(element)
+                console.log(urlHex + findClosestTraitImage(sinergia, traits[sinergia]))
+                element.src = urlHex + findClosestTraitImage(sinergia, traits[sinergia]);
+            })  
         })
-       }
-    },[traits])
+    },[traits, campeonesEnBoard])
 
 
     function handleDrop(e){
         e.preventDefault();
         const hex = e.currentTarget.getElementsByClassName("containerImageChampion");
         var dataCampeon = JSON.parse(e.dataTransfer.getData("campeon"));
+        console.log({dataCampeon})
         const dataFrom = e.dataTransfer.getData("from")
         const containerImageChampion = document.createElement("div");
         containerImageChampion.setAttribute("draggable",true);
@@ -49,14 +184,17 @@ const Builder = ({info="", setInfo=""})=>{
         if(!hex.length && dataFrom === "championList"){
             e.currentTarget.appendChild(containerImageChampion);  
             /* añadimos sinergias*/
+            const existeCampeon = campeonesEnBoard.some(({campeonId}) => {
+                console.log({campeonId, dataCampeonNombre: dataCampeon.nombre})
+                return(campeonId === dataCampeon.nombre)
+            })
+            console.log("Prueba",existeCampeon)
             if(dataCampeon.sinergia.length > 0){
                 const containerSinergias = document.createElement("div"); /* contenedor de todas sinergias*/
                 containerSinergias.className=style.containerSinergias;
                 const lastCount = dataCampeon.sinergia.length - 1;
                 let count = 0;
                 dataCampeon.sinergia.forEach((siner)=>{
-                    console.log({count})
-                    console.log({lastCount})
                     const containerTrait = document.createElement("div"); /* contenedor de 1 sinergia con su background*/
                     containerTrait.className = style.containerTrait;
 
@@ -70,7 +208,7 @@ const Builder = ({info="", setInfo=""})=>{
         
                     const backgroundSinergia = document.createElement("img"); /* background de la sinergia*/
                     backgroundSinergia.classList.add(style.backgroundSinergia);
-                    backgroundSinergia.classList.add(style[siner]);
+                    backgroundSinergia.classList.add(siner);
                     backgroundSinergia.src= urlHex + imgHex[0];
                     backgroundSinergia.alt = imgHex[0];
 
@@ -80,12 +218,12 @@ const Builder = ({info="", setInfo=""})=>{
 
                     /* corrector de suma de aumentos si ya existe el campeon en el tablero*/
 
-                        const existeCampeon = campeonesEnBoard.some((campeon) => campeon === dataCampeon.nombre)
+                        
                         if(!existeCampeon){
                             console.log("no existe campeon");
                             const contadorActual = traits[siner] ? traits[siner] : 0;
-                            setTraits({...traits,[siner]:contadorActual+1});
-                            console.log(siner)
+                            addSinergia(siner,contadorActual);
+ 
 
                     }else{
                         console.log({traits})
@@ -99,7 +237,10 @@ const Builder = ({info="", setInfo=""})=>{
             nombreCampeon.innerHTML=dataCampeon.nombre;
             containerImageChampion.appendChild(nombreCampeon);
             e.currentTarget.getElementsByClassName(style.poligon)[0].style.backgroundColor= championsColor[dataCampeon.coste];
-            setCampeonesEnBoard((oldArray)=>[...oldArray, dataCampeon.nombre])
+            /* ver si la key campeon se cambia por dataCampeon o dataCampeon.nombre solamente*/
+            const hexId = e.target.parentNode.id;
+            addChampionBoard({dataCampeon, hexId})
+            
         }else{
 
             const padre = hex[0].parentNode;
@@ -112,6 +253,23 @@ const Builder = ({info="", setInfo=""})=>{
 
     function handleDragOver(e){
         e.preventDefault();
+    }
+
+    function findClosestTraitImage(traitType, traitLevel) {
+        console.log({traitsColors, traitType, traitLevel});
+        console.log(traitsColors[traitType])
+        if(traitsColors[traitType]){
+            const traitLevels = Object.keys(traitsColors[traitType]).map(Number).sort((a, b) => a - b);
+            console.log("entre")
+            for (let i = traitLevels.length - 1; i >= 0; i--) {
+                if (traitLevel >= traitLevels[i]) {
+                    return traitsColors[traitType][traitLevels[i]];
+                }
+            }
+        
+            // Retornar una imagen por defecto si el traitLevel es menor que todos los disponibles
+        }
+        return "hex-default.webp";
     }
 
     return (
