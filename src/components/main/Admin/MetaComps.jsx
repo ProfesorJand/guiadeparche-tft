ec26504811c2940d4a7c4a629db5b9f0040e3613
profesorjand <profesorjand@gmail.com>
2024-09-13 12:01:25 -0300

    text hotfix 14_18 b

--- src/components/main/Admin/MetaComps.jsx
+++ src/components/main/Admin/MetaComps.jsx
@@ -3,7 +3,7 @@ import { useStore } from "@nanostores/react";
 import Composicion from "./Composicion.jsx";
 import style from "./css/EditarCompoTFT.module.css"
 import {MetaComps as compos, loadCompsMeta} from "src/stores/menuFiltradoAdmin.js"
-const MetaComps = ()=>{
+const MetaComps = ({showHide})=>{
   const composMeta = useStore(compos);
   const admin = localStorage.getItem("superAdmin") || false;
   useEffect(()=>{
@@ -18,7 +18,7 @@ const MetaComps = ()=>{
           {tier.map((compo,i)=>{
             return (
               <div key={`containerMeta`+i} className={style.containerMetaTier}>
-                <Composicion compo={compo}/>
+                <Composicion compo={compo} showHide={showHide} admin={admin}/>
               </div>
             )
           })}
