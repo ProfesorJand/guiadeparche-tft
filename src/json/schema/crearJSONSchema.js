91f68fbe935446696b7e3bbe12036fa69ae1bf7e
profesorjand <profesorjand@gmail.com>
2024-09-18 13:11:40 -0300

    thumbnail e imagen corregidos para motores de busquedas

--- src/json/schema/crearJSONSchema.js
+++ src/json/schema/crearJSONSchema.js
@@ -1,20 +1,33 @@
 import logo from "src/assets/GP_logo.png";
+import { JSDOM } from 'jsdom'; // Importamos jsdom para manipular el DOM en el servidor
 
-function obtenerImagenDinamica() {
-    // Busca la primera imagen que esté visible en la página
-    const imgElement = document.querySelector('img');
+async function obtenerImagenDinamica(urlImagePost) {
+    try {
+        // Recuperamos la página HTML
+        const respuesta = await fetch(urlImagePost);
+        const textoHTML = await respuesta.text();
 
-    // Si existe una imagen, retornamos su URL
-    if (imgElement) {
-        return imgElement.src;
+        // Usamos jsdom para parsear el HTML
+        const dom = new JSDOM(textoHTML);
+        const document = dom.window.document;
+
+        // Busca la primera imagen que esté visible en la página
+        const firstImgElement = document.querySelector('img');
+
+        // Si existe una imagen, retornamos su URL
+        if (firstImgElement) {
+            return firstImgElement.src;
+        }
+
+        // Si no se encuentra ninguna imagen visible, retornamos un valor por defecto
+        return logo.src;
+    } catch (error) {
+        return logo.src; // Si hay un error, devolvemos una imagen por defecto
     }
-
-    // Si no se encuentra ninguna imagen visible, retornamos un valor por defecto
-    return  logo.src;
 }
 
-export function crearJSONSchema({title, imgPost = obtenerImagenDinamica(), thumbnail = logo.src}){
-    //const dateModified = new Date().toISOString();
+export async  function crearJSONSchema({title, thumbnail = logo.src, urlImagePost}){
+    const imgPost = await obtenerImagenDinamica(urlImagePost); // Esperamos a obtener la imagen
     return JSON.stringify({
         "@context": "http://schema.org",
         "@type": "BlogPosting",
