3208747b73097030d319edcc3af1d9e423f5315e
profesorjand <profesorjand@gmail.com>
2024-11-16 21:58:58 -0300

    kick orlando

---
+++ src/components/embed/Kick.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Kick = ()=>{
+  return (
+    <div id="kick-embed"><iframe src="https://player.kick.com/orlandovision" height="720" width="1280" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
+    <style>{`
+      #kick-embed {
+        display: flex;
+        position: relative;
+        box-sizing: border-box;
+        width: 100%;
+        aspect-ratio: 16/9;
+        padding: calc(9rem / 16) 1rem;
+      }
+      `}
+    </style>
+    </div>
+  )
+}
+
+export default Kick;
