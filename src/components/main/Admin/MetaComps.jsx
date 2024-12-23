357f226440d56898ac6a1c8433079e55eba53d97
profesorjand <profesorjand@gmail.com>
2024-12-23 18:38:51 -0300

    composiciones Meta cambio de titulos h2

--- src/components/main/Admin/MetaComps.jsx
+++ src/components/main/Admin/MetaComps.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { useStore } from "@nanostores/react";
 import Composicion from "./Composicion.jsx";
 import style from "./css/EditarCompoTFT.module.css"
@@ -6,42 +6,131 @@ import {MetaComps as compos, loadCompsMeta} from "src/stores/menuFiltradoAdmin.j
 import FantasmaComposiciones from "./FantasmaComposiciones.jsx";
 const MetaComps = ({showHide})=>{
   const composMeta = useStore(compos);
+  const [allFast8, setAllFast8] = useState([]);
+  const [all3Stars, setAll3Stars] = useState([]);
+  const [allAugmentsHero, setAllAugmentsHero] = useState([]);
   const admin = localStorage.getItem("superAdmin") || false;
+  const title = {
+    fast8:"Standard Comps - Fast 8/9",
+    stars3:"Reroll Comps - 3 Star",
+    specificsAugments:"Situational Comps - Specific Auments / Emblem+1"
+  }
+
   useEffect(()=>{
     loadCompsMeta()
   },[])
-  return (
-    <div className={style.containerMeta}>
-      {
-        composMeta.length === 0 &&
-        <>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-          <FantasmaComposiciones></FantasmaComposiciones>
-        </>
-      }
-    {
-      composMeta.length > 0 && composMeta.map((tier,index)=>{
-        return(
-          tier.map((compo,i)=>{
-            return (
-              <div key={`containerMeta`+i} className={[style.containerMetaTier, compo?.isHide && admin ? style.isHideForAdmin : compo?.isHide && !admin ? style.isHide : ""].join(" ")}>
-                <Composicion compo={compo} showHide={showHide} admin={admin}/>
-              </div>
-            )
-          })
-        )
+
+  useEffect(()=>{
+    if(composMeta){
+      const fast8 = [...composMeta].map((tier)=>{
+         return tier.filter((compo)=>{
+          return compo.shadowCategory === "Fast 8"
+         })
       })
+      const stars3 = [...composMeta].map((tier)=>{
+        return tier.filter((compo)=>{
+         return compo.shadowCategory === "3 Stars"
+        })
+      })
+      const SpecificsAugments = [...composMeta].map((tier)=>{
+        return tier.filter((compo)=>{
+         return compo.shadowCategory === "Specifics Augments"
+        })
+      })
+      
+      setAllFast8(fast8)
+      setAll3Stars(stars3)
+      setAllAugmentsHero(SpecificsAugments)
     }
-    </div>
-  )
+  },[composMeta])
+
+  useEffect(()=>{
+    console.log({allFast8})
+    console.log({all3Stars})
+    console.log({allAugmentsHero})
+
+  },[all3Stars, allFast8, allAugmentsHero])
+
+  if(composMeta.length === 0){
+    return (
+      <div className={style.containerMeta}>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+        <FantasmaComposiciones></FantasmaComposiciones>
+      </div>
+    )
+  }
+
+  if(allFast8 || all3Stars || allAugmentsHero){
+    return (
+      <div className={style.containerMeta}>
+        {allFast8.length > 0 && 
+        <>
+        <h2>{title.fast8}</h2>
+        {
+        allFast8.map((tier,index)=>{
+          return (
+            tier.map((compo,i)=>{
+              return (
+                <div key={`containerMetaAugmentsHero`+i} className={[style.containerMetaTier, compo?.isHide && admin ? style.isHideForAdmin : compo?.isHide && !admin ? style.isHide : ""].join(" ")}>
+                  <Composicion compo={compo} showHide={showHide} admin={admin}/>
+                </div>
+              )
+            })
+          )
+        })
+      }
+      </>
+      }
+      {all3Stars.length > 0 && 
+        <>
+        <h2>{title.stars3}</h2>
+        {
+        all3Stars.map((tier,index)=>{
+          return (
+            tier.map((compo,i)=>{
+              return (
+                <div key={`containerMetaAugmentsHero`+i} className={[style.containerMetaTier, compo?.isHide && admin ? style.isHideForAdmin : compo?.isHide && !admin ? style.isHide : ""].join(" ")}>
+                  <Composicion compo={compo} showHide={showHide} admin={admin}/>
+                </div>
+              )
+            })
+          )
+        })
+      }
+      </>
+      }
+      {allAugmentsHero.length > 0 && 
+        <>
+        <h2>{title.specificsAugments}</h2>
+        {
+        allAugmentsHero.map((tier,index)=>{
+          return (
+            tier.map((compo,i)=>{
+              return (
+                <div key={`containerMetaAugmentsHero`+i} className={[style.containerMetaTier, compo?.isHide && admin ? style.isHideForAdmin : compo?.isHide && !admin ? style.isHide : ""].join(" ")}>
+                  <Composicion compo={compo} showHide={showHide} admin={admin}/>
+                </div>
+              )
+            })
+          )
+        })
+      }
+      </>
+      }
+
+      </div>
+    )
+  }
+
+  
 }
 
 export default MetaComps;
