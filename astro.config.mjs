0a2d024454da33987de40b1d93790690564ad561
profesorjand <profesorjand@gmail.com>
2025-06-16 13:16:25 -0300

    Configura Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,26 +6,33 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://guiadeparche.com', // para guiadeparche
+//   //site: 'https://profesorjand.github.io', // para github page
+//   //base: '/guiadeparche-tft', // para github page
+//   //trailingSlash: "always", //para github page
+//   redirects: {
+//     '/': '/tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/' // github page '/guiadeparche-tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/'
+//   },
+//   output: 'static', // Cambiado a 'static' para asegurarse de que se genera contenido estático para deploy
+//   //output: 'server',
+//   // adapter: node({
+//   //   mode: '@astrojs/node',
+//   // }),
+//   adapter: node({
+//     mode: 'standalone',
+//   }),
+//   image: {
+//     // remotePatterns: [{ protocol: "https" }],
+//     service: squooshImageService(),
+//     domains: ['astro.build'],
+//   },
+//   integrations: [db(), react(), sitemap()]
+// });
+
 export default defineConfig({
-  site: 'https://guiadeparche.com', // para guiadeparche
-  //site: 'https://profesorjand.github.io', // para github page
-  //base: '/guiadeparche-tft', // para github page
-  //trailingSlash: "always", //para github page
-  redirects: {
-    '/': '/tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/' // github page '/guiadeparche-tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/'
-  },
-  output: 'static', // Cambiado a 'static' para asegurarse de que se genera contenido estático para deploy
-  //output: 'server',
-  // adapter: node({
-  //   mode: '@astrojs/node',
-  // }),
-  adapter: node({
-    mode: 'standalone',
-  }),
-  image: {
-    // remotePatterns: [{ protocol: "https" }],
-    service: squooshImageService(),
-    domains: ['astro.build'],
-  },
-  integrations: [db(), react(), sitemap()]
+  site: 'https://guiadeparche.com',
+  base: '/', // muy importante que NO tenga subcarpetas
+  output: 'static',
+  integrations: [react(), sitemap(),db()]
 });
