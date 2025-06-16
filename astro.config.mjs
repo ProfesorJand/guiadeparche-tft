ab2b0e8e86ffd8fe48c561134162ec388d13ab60
profesorjand <profesorjand@gmail.com>
2025-06-16 11:33:45 -0300

    asdasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,34 +6,24 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 // https://astro.build/config
-// export default defineConfig({
-//   site: 'https://guiadeparche.com', // para guiadeparche
-//   //site: 'https://profesorjand.github.io', // para github page
-//   //base: '/guiadeparche-tft', // para github page
-//   //trailingSlash: "always", //para github page
-//   redirects: {
-//     '/': '/tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/' // github page '/guiadeparche-tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/'
-//   },
-//   output: 'static', // Cambiado a 'static' para asegurarse de que se genera contenido estático para deploy
-//   //output: 'server',
-//   // adapter: node({
-//   //   mode: '@astrojs/node',
-//   // }),
-//   // adapter: node({
-//   //   mode: 'standalone',
-//   // }),
-//   image: {
-//     // remotePatterns: [{ protocol: "https" }],
-//     service: squooshImageService(),
-//     domains: ['astro.build'],
-//   },
-//   integrations: [db(), react(), sitemap()]
-// });
-
 export default defineConfig({
-  site: 'https://guiadeparche.com',
-  output: 'static',
+  site: 'https://guiadeparche.com', // para guiadeparche
+  //site: 'https://profesorjand.github.io', // para github page
+  //base: '/guiadeparche-tft', // para github page
+  //trailingSlash: "always", //para github page
+  redirects: {
+    '/': '/tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/' // github page '/guiadeparche-tft/composiciones-tft/mejores-composiciones-de-teamfight-tactics/'
+  },
+  output: 'static', // Cambiado a 'static' para asegurarse de que se genera contenido estático para deploy
+  //output: 'server',
+  // adapter: node({
+  //   mode: '@astrojs/node',
+  // }),
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
   image: {
+    // remotePatterns: [{ protocol: "https" }],
     service: squooshImageService(),
     domains: ['astro.build'],
   },
