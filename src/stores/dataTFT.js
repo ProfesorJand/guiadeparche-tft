46cd7bd37acf3f0fa98b30c3f6415bf2b92ba355
profesorjand <profesorjand@gmail.com>
2026-08-22 12:52:29 -0300

    cambiando al set 18 para que se muestre de primero y ajustes css a master plan

--- src/stores/dataTFT.js
+++ src/stores/dataTFT.js
@@ -14,7 +14,7 @@ const initialStateDataTFTSets = [];
 const initialStateDataTFTChampions = [];
 const initialStateVersion = "pbe";
 const initialStateTeamPlannerCode = [];
-export const initialTFT_SET = "latest";
+export const initialTFT_SET = "pbe";
 
 export const setNumberPBE = "18";
 export const setMutatorPBE = "TFTSet18";
